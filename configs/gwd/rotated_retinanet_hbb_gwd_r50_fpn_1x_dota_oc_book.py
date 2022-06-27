59a7aeb5e220e07c8c61d078bebd3f466a0ab016
Albin <albin.zhu.me@gmail.com>
2022-06-27 11:48:09 +0800

    bug rfix

--- configs/gwd/rotated_retinanet_hbb_gwd_r50_fpn_1x_dota_oc_book.py
+++ configs/gwd/rotated_retinanet_hbb_gwd_r50_fpn_1x_dota_oc_book.py
@@ -2,7 +2,7 @@
 Author: bin.zhu
 Date: 2022-06-27 11:25:46
 LastEditors: bin.zhu
-LastEditTime: 2022-06-27 11:39:55
+LastEditTime: 2022-06-27 11:47:50
 Description: file content
 '''
 _base_ = ['../rotated_retinanet/rotated_retinanet_hbb_r50_fpn_1x_dota_oc.py']
@@ -12,8 +12,8 @@ classes = ('left', 'right')
 model = dict(
     bbox_head=dict(
         reg_decoded_bbox=True,
-        loss_bbox=dict(
-            type='GDLoss', loss_type='gwd', loss_weight=5.0, num_classes=2)))
+        loss_bbox=dict(type='GDLoss', loss_type='gwd', loss_weight=5.0),
+        num_classes=2))
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
